1fc0b7f2972f33548c22728bf6b4f4f81583eddc
swarnadipd <91596416+swarnadipd@users.noreply.github.com>
2023-04-25 15:30:04 +0530

    stTable

---
+++ studentTable.php
@@ -0,0 +1,49 @@
+<?php
+include("nav.php");
+?>
+
+<section>
+
+  <h1>Student Table</h1>
+  <div class="tbl-header">
+    <table cellpadding="0" cellspacing="0" border="0">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Phone no.</th>
+          <th>Email</th>
+          <th>Class</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+  <div class="tbl-content">
+    <table cellpadding="0" cellspacing="0" border="0">
+      <tbody>
+        <tr id="info_h">
+          <td id="info_hl">Swarnadip</td>
+          <td>0987654321 </td>
+          <td>loverboy69@harder.com</td>
+          <td>12</td>
+          <td id="info_hr"></td>
+        </tr>
+        <tr id="info_h">
+          <td>mia boudi</td>
+          <td>6969696969</td>
+          <td>omago@arojore.com</td>
+          <td>69</td>
+          <td></td>
+        </tr>
+        
+      </tbody>
+    </table>
+  </div>
+</section>
+
+
+
+
+<?php
+include("footer.php");
+?>
